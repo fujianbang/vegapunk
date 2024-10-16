21fb2df704bb1e0a90b5efe0a150e1d274335829
Jianbang <35134798+fujianbang@users.noreply.github.com>
2024-10-16 22:27:48 +0800

    feat: add AddHostModal component and integrate in hosts layout

--- src/routes/hosts/+layout.svelte
+++ src/routes/hosts/+layout.svelte
@@ -1,3 +1,23 @@
+<script lang="ts">
+  import AddHostModal from "./AddHostModal.svelte";
+
+  let modalOpen = false;
+
+  function showModal() {
+    modalOpen = true;
+  }
+</script>
+
 <div class="h-screen">
+  <div class="p-6">
+    <button
+      type="button"
+      on:click={showModal}
+      class="rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+      >Add Host</button
+    >
+  </div>
   <slot />
 </div>
+
+<AddHostModal bind:open={modalOpen} />
