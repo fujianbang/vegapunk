b1a6f0c0b7db4fd8ccff2750f66e559665622a3e
Jianbang <35134798+fujianbang@users.noreply.github.com>
2024-12-02 23:54:58 +0800

    feat: add get_hosts command and update invoke handler

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -10,7 +10,11 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_websocket::init())
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet, commands::add_new_host])
+        .invoke_handler(tauri::generate_handler![
+            greet,
+            commands::add_new_host,
+            commands::get_hosts
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
