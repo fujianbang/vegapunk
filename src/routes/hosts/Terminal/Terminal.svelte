2316f03aba3457fac1e518256e53ebce662093a4
Jianbang <35134798+fujianbang@users.noreply.github.com>
2025-02-13 02:01:55 +0800

    refactor: Clean up Terminal.svelte code formatting and use consistent sessionId variable naming.

--- src/routes/hosts/Terminal/Terminal.svelte
+++ src/routes/hosts/Terminal/Terminal.svelte
@@ -1,61 +1,62 @@
 <script lang="ts">
-    import { Terminal } from "@xterm/xterm";
-    import { FitAddon } from "@xterm/addon-fit";
-    import { onMount, onDestroy } from "svelte";
-    import "@xterm/xterm/css/xterm.css";
-    import { Channel, invoke } from "@tauri-apps/api/core";
-    import { listen } from "@tauri-apps/api/event";
-    
+  import { Terminal } from "@xterm/xterm";
+  import { FitAddon } from "@xterm/addon-fit";
+  import { onMount, onDestroy } from "svelte";
+  import "@xterm/xterm/css/xterm.css";
+  import { Channel, invoke } from "@tauri-apps/api/core";
+  import { listen } from "@tauri-apps/api/event";
 
-    let terminalElement: HTMLElement;
-    let terminal: Terminal;
+  let terminalElement: HTMLElement;
+  let terminal: Terminal;
 
-    interface SshEvent {
-        event: "data";
-        data: string;
-    }
+  interface SshEvent {
+    event: "data";
+    data: string;
+  }
 
-    async function initTerminal() {
-        if (!terminalElement) return;
+  async function initTerminal() {
+    if (!terminalElement) return;
 
-        terminal = new Terminal({
-            cursorBlink: true,
-            fontSize: 14,
-            theme: {
-                foreground: "#e0e1e4",
-                background: "#181818",
-            },
-        });
-
-        const fitAddon = new FitAddon();
-        terminal.loadAddon(fitAddon);
-        terminal.open(terminalElement);
-        fitAddon.fit();
-
-        // create channel
-        const channel = new Channel<SshEvent>();
-
-        channel.onmessage = (event: SshEvent) => {
-            console.log("got data", event);
-            terminal.write(event.data);
-        };
-
-        // invoke("create_ssh_connection");
-
-        console.log("ready to listen");
-        invoke("listen_ssh_data", { sessionId: "2222-test", channel });
-
-        terminal.onData((data) => {
-            console.log(data);
-            invoke("communicate", { session_id: "communicate-test", data });
-        });
-
-        window.addEventListener("resize", () => fitAddon.fit());
-    }
-
-    onMount(() => {
-        initTerminal();
+    terminal = new Terminal({
+      cursorBlink: true,
+      fontSize: 14,
+      theme: {
+        foreground: "#e0e1e4",
+        background: "#181818",
+      },
     });
+
+    const fitAddon = new FitAddon();
+    terminal.loadAddon(fitAddon);
+    terminal.open(terminalElement);
+    fitAddon.fit();
+
+    // create channel
+    const channel = new Channel<SshEvent>();
+
+    channel.onmessage = (event: SshEvent) => {
+      console.log("got data", event);
+      terminal.write(event.data);
+    };
+
+    // invoke("create_ssh_connection");
+
+    const mockSessionID = "2222-test";
+
+    console.log("ready to listen");
+    invoke("listen_ssh_data", { sessionId: mockSessionID, channel });
+
+    terminal.onData((data) => {
+      console.log(data);
+      invoke("communicate", { sessionId: mockSessionID, data });
+    });
+
+    window.addEventListener("resize", () => fitAddon.fit());
+  }
+
+  onMount(() => {
+    initTerminal();
+  });
 </script>
 
 <div class="h-full" bind:this={terminalElement}></div>
